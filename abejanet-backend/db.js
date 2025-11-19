792512b45a1cb542ec23bf68940032e9d1f719d0
Victor <victorlopezcebreros117@gmail.com>
2025-11-18 22:48:37 -0600

    ssl

--- abejanet-backend/db.js
+++ abejanet-backend/db.js
@@ -13,6 +13,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false },
 
   // opciones del pool
   max: 10,                 // conexiones máximas
